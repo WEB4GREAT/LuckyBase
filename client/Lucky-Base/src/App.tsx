cf1ca89da8a3b1ed0249e52c6f958e2b2db4621c
Oshodi Alola <lilwade4000@gmail.com>
2025-07-13 13:04:36 +0100

    Version 0.01

---
+++ client/Lucky-Base/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container flex-1/2 w-full h-full space-x-0">
+        <h1 className = "font-black size-15">LUCKY BASE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
